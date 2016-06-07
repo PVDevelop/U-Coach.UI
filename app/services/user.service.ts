4b6cad81d623472eb69ff09c56f7932feb8c05ec
Ilya Prokofyev <beetlewar@mail.ru>
2016-06-07 17:29:37 +0300

    реализовал auth.post

--- app/services/user.service.ts
+++ app/services/user.service.ts
@@ -1,9 +1,10 @@
 import {Injectable}         from '@angular/core';
-import {Http, Response}     from '@angular/http';
+import {Http, Response, Headers}     from '@angular/http';
 import {Observable}         from 'rxjs/Observable';
+import {Serializer}         from 'serializer.ts/Serializer';
+
 import {Logon}              from '../classes/logon/logon';
 import {User}               from '../classes/user/user';
-import {Serializer}         from 'serializer.ts/Serializer';
 
 @Injectable()
 export class UserService{
@@ -15,8 +16,17 @@ export class UserService{
     public logon(logon: Logon){
 
         let json = JSON.stringify(logon);
-        alert("Logon: " + json);
-
+        
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        
+        this.http.
+        post("http://localhost:7788/api/auth", json, { headers: headers }).
+        subscribe(
+            null, 
+            err => console.error("Error on post auth: " + err),
+            () => console.log("Auth complete"));
+            
         // this.http.post('http://localhost:7788/api/auth').
         // map(this.mapUsers).
         // subscribe(
