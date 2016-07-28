31e805398d73e321f16a1df0f71bfaa7d781ce73
Илья Прокофьев < >
2016-07-28 09:45:29 +0300

    Реализовал put на логон, cookie пока не работают

--- app/services/user.service.ts
+++ app/services/user.service.ts
@@ -2,6 +2,7 @@ import {Injectable}         from '@angular/core';
 import {Http, Response, Headers}     from '@angular/http';
 import {Observable}         from 'rxjs/Observable';
 import {Serializer}         from 'serializer.ts/Serializer';
+import {CookieService}      from 'angular2-cookie/core';
 
 import {Logon}              from '../classes/logon/logon';
 import {User}               from '../classes/user/user';
@@ -10,21 +11,25 @@ import {User}               from '../classes/user/user';
 export class UserService{
     constructor(
         private http: Http,
-        private serializer: Serializer){
+        private serializer: Serializer,
+        private cookieService: CookieService){
     }
     
     public logon(logon: Logon){
-
         let json = JSON.stringify(logon);
         
         var headers = new Headers();
         headers.append('Content-Type', 'application/json');
+
+        var uri =
+            "http://localhost:51668/api/ucoach/logon"; 
         
-        this.http.
-        post("http://localhost:7788/api/auth", json, { headers: headers }).
-        subscribe(
-            null, 
-            err => console.error("Error on post auth: " + err),
-            () => console.log("Auth complete"));
+        this.
+            http.
+            put(uri, json, { headers: headers }).
+            subscribe(
+                null, 
+                err => console.error("Error on ucoach logon: " + err),
+                () => console.log("Ucoach logon succeeded"));
     }
 }
